893eb610fb09218fbea6a541e68622d3b893728b
Joailys <simon0lemaitre@gmail.com>
2021-04-08 15:42:42 +0200

    crea du script JS et crea du haeder

---
+++ func.js
@@ -0,0 +1,13 @@
+function imgGalery()
+{
+    var active = $('#galery .active');
+
+    var next = (active.next().length > 0) ? active.next() : $('#galery img:first');
+
+        active.fadeOut(function(){
+            active.removeClass('active');
+            next.fadeIn().addClass('active');
+        });
+}
+setInterval('imgGalery()',2500);
+imgGalery();
